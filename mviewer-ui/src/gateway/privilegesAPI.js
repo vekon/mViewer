d8fec944b642086cbbc7bd0f5d6deed1d8056dc2
venkoux <venukonda.g@gmail.com>
2016-10-24 15:14:57 +0530

    Initial privileges setup

---
+++ mviewer-ui/src/gateway/privilegesAPI.js
@@ -0,0 +1,34 @@
+var privileges;
+
+function setRolesAndPrivileges(privs) {
+  privileges = privs;
+  console.log('privileges : ' + privileges);
+}
+
+function hasPrivilege(privilege,collection,db) {
+  var privs = privileges.inheritedPrivileges.filter(function(eachPriv){
+    return eachPriv.resource.collection == collection && eachPriv.resource.db == db;
+  });
+
+  if (privs.length == 0) {
+    privs = privileges.inheritedPrivileges.filter(function(eachPriv){
+      return eachPriv.resource.collection == "" && eachPriv.resource.db == db;
+    });
+  }
+
+  var actions;
+  if (privs && privs.length > 0) {
+    actions = privs[0].actions.filter(function(eachAction){
+      return eachAction == privilege;
+    });
+  }
+
+  return actions && actions.length > 0;
+}
+
+var privilegesAPI = {
+  setRoles: setRolesAndPrivileges,
+  hasPrivilege: hasPrivilege
+}
+
+export default privilegesAPI;
