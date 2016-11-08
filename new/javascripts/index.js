d33a42d37870761dc6f700e14e28ba8381bc9fde
venkoux <venukonda.g@gmail.com>
2016-11-08 18:39:33 +0530

    Feaute page changes

---
+++ new/javascripts/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+  	infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  dots: true,
+	  adaptiveHeight: true,
+	  arrows:true
+  });
+});
