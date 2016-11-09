0b5be7b66c883cbe5630a56cb87a9f27a472080a
venkoux <venukonda.g@gmail.com>
2016-11-09 22:59:08 +0530

    Feature page changes

--- new/javascripts/index.js
+++ new/javascripts/index.js
@@ -1,19 +1,8 @@
 $(document).ready(function(){
-  $('.slider').slick({
-  	infinite: true,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  dots: true,
-	  adaptiveHeight: true,
-	  arrows:true
-  });
-
 	$('[data-toggle=popover]').popover({
 		content: $('#downloads').html(),
 		html: true
 	}).click(function() {
 	    $(this).popover('show');
 	});
-
-
 });
