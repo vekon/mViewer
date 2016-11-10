cdc17b04b0742d1176bfd71c7ac51b0086196789
hemanth kumar potnuri <hemanthkumarpotnuri@gmail.com>
2016-11-10 17:52:28 +0530

    banner and css changes

--- new/javascripts/documentation.js
+++ new/javascripts/documentation.js
@@ -1,32 +1,41 @@
 $(document).ready(function () {
 
-$('#sidebar li').on('click',function(){
-  $('#sidebar li').removeClass('active');
-  $(this).addClass('active');
-  $("html, body").animate({ scrollTop: 0 }, "slow");
+  $('#sidebar li').on('click',function(){
+    $('#sidebar li').removeClass('active');
+    $(this).addClass('active');
+    $("html, body").animate({ scrollTop: 0 }, "slow");
 
-});
-
-
-$('#toggle-buttom').on('click',function(){
-  $('.page-holder').toggleClass('pull-down');
-  $('.side-nav').toggleClass('pull-down');
-});
+  });
 
 
+  $('iframe').iFrameResize({ log : false, inPageLinks : true}) ;
 
-$('iframe').iFrameResize({ log : false, inPageLinks : true}) ;
+
+  $('[data-toggle=popover]').popover({
+      content: $('#downloads').html(),
+      html: true
+  }).click(function() {
+      $(this).popover('show');
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() == 0) {
+        $('header').css({
+                'box-shadow': 'none',
+                '-moz-box-shadow' : 'none',
+                '-webkit-box-shadow' : 'none' });
+    }
+    else {
+        $('header').css({
+                'box-shadow': '0px 4px 6px #888888',
+                '-moz-box-shadow' : '0px 4px 6px #888888',
+                '-webkit-box-shadow' : '0px 4px 6px #888888' });
+    }
+  });
 
 
-$('[data-toggle=popover]').popover({
-    content: $('#downloads').html(),
-    html: true
-}).click(function() {
-    $(this).popover('show');
-});
-
-$(document).scroll(function () {
-	var navWrap = $('#navWrap'),
+  $(document).scroll(function () {
+  	var navWrap = $('#navWrap'),
     nav = $('.bs-docs-sidebar>ul'),
     startPosition = navWrap.offset().top,
     stopPosition = $('#stopHere').offset().top - nav.outerHeight();
@@ -35,7 +44,7 @@ $(document).scroll(function () {
     
     if (y > startPosition) {
         nav.addClass('sticky');
-		if (y > (stopPosition - 125)) {
+  	if (y > (stopPosition - 125)) {
             nav.css('top', stopPosition -125 - y);
         } else {
             nav.css('top', 20);
@@ -44,7 +53,7 @@ $(document).scroll(function () {
     } else {
         nav.removeClass('sticky');
     } 
-});
+  });
 
 });
 
--- new/javascripts/index.js
+++ new/javascripts/index.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
 	}).click(function() {
 	    $(this).popover('show');
 	});
+
+
+	$(window).scroll(function() {
+    if ($(this).scrollTop() == 0) {
+        $('header').css({
+                'box-shadow': 'none',
+                '-moz-box-shadow' : 'none',
+                '-webkit-box-shadow' : 'none' });
+    }
+    else {
+        $('header').css({
+                'box-shadow': '0px 4px 6px #888888',
+                '-moz-box-shadow' : '0px 4px 6px #888888',
+                '-webkit-box-shadow' : '0px 4px 6px #888888' });
+    	}
+	});
+
 });
+
+
