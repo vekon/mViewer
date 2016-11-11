462ad0eba2ba11d0b30ad2229edf8f0e7efea2e0
hemanth potnuri <hemanthkumarpotnuri@gmail.com>
2016-11-11 14:00:35 +0530

    fixed typo in home page

--- mviewer-ui/src/components/createdb/CreateDbComponent.jsx
+++ mviewer-ui/src/components/createdb/CreateDbComponent.jsx
@@ -135,7 +135,7 @@ class CreateDbComponent extends React.Component {
       <div className={createDbStyles.mainContainer}>
         <div className = {createDbStyles.topContainer}>
           <section className={createDbStyles.topSection}>WELCOME TO MVIEWER</section>
-          <section className={createDbStyles.midSection}>A MONOGODB MANAGEMENT TOOL</section>
+          <section className={createDbStyles.midSection}>A MONGODB MANAGEMENT TOOL</section>
           <section className={createDbStyles.bottomSection}>LET'S GET STARTED</section>
         </div>
         <div className = {createDbStyles.bottomContainer}>
